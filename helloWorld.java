d9c63675008f00373b07ba516d6ae353f1b6e742
Himanshu Vyas <HVyas@ad.prolifics.com>
2017-05-30 19:13:26 +0530

    ye apna style h

---
+++ helloWorld.java
@@ -0,0 +1,16 @@
+public class helloWorld{
+	
+	public static void main(String[] args){
+		
+		for(int i=1;i<=5;i++)
+		{
+		System.out.println("hi world this is yoo"+i);	
+		
+		}
+		
+		
+		
+	}
+	
+	
+}
